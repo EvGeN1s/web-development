8ea94cda50b2ee386e3c9b2c9438c6e815b7d9ec
EvJune Федосеев <evgeniy.fedoseev.2002@gmail.com>
2021-03-03 14:53:27 +0300

    project ready

--- lw3/task1/index.php
+++ lw3/task1/index.php
@@ -1,1 +1,26 @@
+<? php
 
+function getQueryStringParameter(string $name): ?string
+{
+    return isset($_GET[$name]) ? $_GET[$name] : null;
+}
+
+getQueryStringParameter($text);
+echo('Исходная строка: ' . $text);
+for ($i = 0,  $i < strlen($text), $i++)
+{
+        $temp;
+        if $text[$i] !== ' '
+        {
+                $temp += $text[$i];
+        } 
+        else
+        {
+                if ($text[$i + 1] !== ' ') 
+                {
+                    $temp += ' ';
+                }
+        }
+        $text = $temp;
+}
+echo($temp);
