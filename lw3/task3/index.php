df1acfd483f0370a7ea2a10243d1ebbbe7bf1637
EvGeN1s <evgeniy.fedoseev.2002@gmail.com>
2021-03-07 13:24:33 +0300

    Block for finding strebgth removed to if block

--- lw3/task3/index.php
+++ lw3/task3/index.php
@@ -5,32 +5,29 @@ $patern_password = '/^[a-zA-z\d]*$/';
 $password = getQueryStringParameter('password');
 if (preg_match($patern_password, $password))
 {
-
+    $strength = 4 * strlen($password);
+    $strength += 4 * preg_match_all('/\d/', $password);
+    $strength += ((strlen($password) - preg_match_all('/[A-Z]/', $password))* 2);
+    $strength += ((strlen($password) - preg_match_all('/[a-z]/', $password)) * 2);
+    if (preg_match('/^\d+$/', $password))
+    {
+        $strength -= strlen($password);
+    }
+    if (preg_match('/^[a-zA-Z]+$/', $password))
+    {
+        $strength -= strlen($password);
+    }
+    foreach (count_chars($password, 1) as $number)
+    {
+        $number > 1 ? $strength-- : null;
+    }
+    echo('password strength: ' . $strength);
 }
 else
 {
     echo('Incorrect password');
     
 }
-
-$strength = 4 * strlen($password);
-$strength += 4 * preg_match_all('/\d/', $password);
-$strength += ((strlen($password) - preg_match_all('/[A-Z]/', $password))* 2);
-$strength += ((strlen($password) - preg_match_all('/[a-z]/', $password)) * 2);
-if (preg_match('/^\d+$/', $password))
-{
-    $strength -= strlen($password);
-}
-if (preg_match('/^[a-zA-Z]+$/', $password))
-{
-    $strength -= strlen($password);
-}
-foreach (count_chars($password, 1) as $number)
-{
-    $number > 1 ? $strength-- : null;
-}
-
-echo('password strength: ' . $strength);
 
 function getQueryStringParameter(string $name): ?string
 {
